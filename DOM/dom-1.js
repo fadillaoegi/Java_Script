2fdebfe9cc742932f83a06cbd043a8787cf95104
Fadil <fadilfdl2710@gmail.com>
2022-02-28 17:37:35 +0700

    Update DOM

--- DOM/dom-1.js
+++ DOM/dom-1.js
@@ -13,4 +13,22 @@ title.style.backgroundColor = "black";
 title.innerHTML = "Hello Broo !!";
 
 // -> document.getElementByTagName();
+// HTMLCollection -> Array
 
+const data2 = document.getElementsByTagName('p');
+
+for (let index = 0; index < data2.length; index++) {
+    
+    data2[index].style.backgroundColor = "lightblue";
+    
+}
+
+const data3 = document.getElementsByTagName('h1')[0];
+data3.style.fontSize = "70px";
+
+// -> document.getElementByClassName()
+// HTMLCollection -> Array
+
+const data4 = document.getElementsByClassName('p1');
+
+data4[0].innerHTML = "Ini Berubah Brooo, ya kaa ??";
