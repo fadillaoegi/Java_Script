3d7803a4a56c88924fcbecc78af6cce4de6b01a4
Fadil <fadilfdl2710@gmail.com>
2022-02-28 17:13:21 +0700

    Update document.getElementById()

--- DOM/dom-1.js
+++ DOM/dom-1.js
@@ -1,1 +1,16 @@
-console.log(document);
+// console.log(document);
+
+// DOM selection
+
+// -> document.getElementById()
+
+const data = document.getElementById('title');
+// Merubah warna
+title.style.color = "blue";
+// Merubah Background
+title.style.backgroundColor = "black";
+// Merubah Text
+title.innerHTML = "Hello Broo !!";
+
+// -> document.getElementByTagName();
+
