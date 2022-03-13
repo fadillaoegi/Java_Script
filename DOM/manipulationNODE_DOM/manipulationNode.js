ec046457d99e83a5d36314eb25dea9587d43f9f0
Fadil <fadilfdl2710@gmail.com>
2022-03-13 10:22:55 +0700

    Update

--- DOM/manipulationNODE_DOM/manipulationNode.js
+++ DOM/manipulationNODE_DOM/manipulationNode.js
@@ -5,7 +5,7 @@
 // node.appendChild();
 // node.insertBefore();
 // parentNode.removeChild();
-// paprentNode.removeChild();
+// paprentNode.replaceChild();
 
 // Make Element
 const tag = document.createElement('p');
@@ -16,6 +16,7 @@ tag.appendChild(value);
 
 // Menampilkan -> Browser
 const sectionB = document.getElementById('b');
+// node.appendChild();
 sectionB.appendChild(tag);
 
 const tagLi = document.createElement('li');
@@ -27,8 +28,29 @@ const ul = document.querySelector('section#b ul');
 // jika tidak mempunyai attribute pakai sudo class -> nth-child(value);
 //  -> nth-child = dimulai dari 1 (bukan 0)
 const li2 = ul.querySelector('li:nth-child(2)');
+// node.insertBefore();
+ul.insertBefore(tagLi, li2);
 
-ul.insertBefore(valueLi, li2);
+const sectionA = document.getElementById('a');
+// document.getElementsByTagName() -> jadi sebuah array
+const link = document.getElementsByTagName('a')[0];
+// parentNode.removeChild();
+sectionA.removeChild(link);
+
+const tagH2 = document.createElement('h2');
+const valueH2 = document.createTextNode('Transform');
+const tagP4 = sectionB.querySelector('p');
+
+tagH2.appendChild(valueH2);
+
+sectionB.replaceChild(tagH2, tagP4);
+
+
+tagH2.style.backgroundColor = 'salmon';
+li2.style.backgroundColor = 'salmon';
+tag.style.backgroundColor = 'salmon';
+
+
 
 
 
